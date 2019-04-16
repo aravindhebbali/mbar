e1143971f33f5f5c8670251cef211e432b8ac873
rsquaredin <rsquaredin@gmail.com>
2019-04-16 16:28:10 +0530

    add prep_data function

---
+++ R/mbar-prep-data.R
@@ -0,0 +1,54 @@
+#' Prepare Data
+#'
+#' Prepares transaction data for analysis.
+#'
+#' @param data A \code{data.frame} or \code{tibble}.
+#' @param id Unique id for the transaction.
+#' @param Product/items part of the transaction.
+#'
+#' @importFrom magrittr %>% %<>%
+#'
+#' @export
+#'
+mbar_prep_data <- function(data, id, products) {
+
+  col_id <- rlang::enquo(id)
+  col_items <- rlang::enquo(products)
+
+  prep_data <-
+    data %>%
+    dplyr::select(!! col_id, !! col_items) %>%
+    dplyr::group_by(!! col_id)
+
+  group_items <-
+    prep_data %>%
+    dplyr::mutate(items = paste(!! col_items, collapse = ',')) %>%
+    dplyr::select(-!! col_items) %>%
+    dplyr::distinct()
+
+  count_items <-
+    prep_data %>%
+    dplyr::tally()
+
+  max_items <-
+    count_items %>%
+    dplyr::pull(n) %>%
+    max()
+
+  result <-
+    group_items %>%
+    tidyr::separate(items, into = paste0("item_", seq_len(max_items)), sep = ",") %>%
+    dplyr::inner_join(count_items, by = rlang::quo_name(col_id)) %>%
+    dplyr::arrange(n) %>%
+    dplyr::select(-n)
+
+
+  replace_na_list <- as.list(rep("", max_items))
+  names(replace_na_list) <- names(result)[-1]
+
+
+  result %<>%
+    tidyr::replace_na(replace_na_list)
+
+  return(result)
+}
