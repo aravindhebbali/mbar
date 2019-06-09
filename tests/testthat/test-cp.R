f41cf7e2711b0d5fd6e89f671c54a9eab4c649c5
rsquaredin <rsquaredin@gmail.com>
2019-06-09 23:16:16 +0530

    add tests

--- tests/testthat/test-cp.R
+++ tests/testthat/test-cp.R
@@ -4,3 +4,8 @@ test_that("output from optimal_cp is as expected", {
   model <- rpart::rpart(Species ~ ., data = iris)
   expect_equal(optimal_cp(model), 0.01)
 })
+
+test_that("optimal_cp throws appropriate error messages", {
+	model <- lm(mpg ~ ., data = mtcars)
+	expect_error(optimal_cp(model), "model must be an object of class rpart")
+})
